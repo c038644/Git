24472d1a98427af58c89e9c767df1e39bb97c1d4
c038644 <106696284+c038644@users.noreply.github.com>
2022-10-12 11:56:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,9 @@ with st.spinner('Updating Report...'):
     
     #global_graph_df = json.loads(global_graph)
     
-    global_graph_df = pd.DataFrame.from_dict('global_graph')
+    global_graph
+    
+    global_graph_df = pd.DataFrame.from_dict(global_graph)
     
     global_graph_df
     

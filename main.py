058465a35e9977094528206387a97e6c8e8edf46
c038644 <106696284+c038644@users.noreply.github.com>
2022-10-13 06:31:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,13 +36,18 @@ with st.spinner('Updating Report...'):
         Selected_Customer = all_data.loc[all_data['SK_ID_CURR'] == Customer]
         st.write(Selected_Customer)
         Selected_Customer.to_csv("files/selection.csv")
-        local = requests.get("https://c038644.herokuapp.com/local").json()
-        local_graph_df = pd.DataFrame.from_dict(local)
-        local_graph_df
+        #local = requests.get("https://c038644.herokuapp.com/local").json()
+        #local_graph_df = pd.DataFrame.from_dict(local)
+        #local_graph_df
         #local = requests.post("http://127.0.0.1:5000/local", Selected_Customer)
         #local_graph_df = pd.DataFrame.from_dict('local', orient="index")
         #local_graph_df 
-       
+    
+   
+    local = requests.get("https://c038644.herokuapp.com/local").json()
+    local_graph_df = pd.DataFrame.from_dict(local)
+    local_graph_df
+    
     g1, g2, g3 = st.columns((1,1,1))
 
     #local_graph_df = pd.read_csv("files/Customer_score.csv")

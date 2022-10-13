9b6aa105c8d641a19ede2c77ce6cada8dfb8f9a9
c038644 <106696284+c038644@users.noreply.github.com>
2022-10-13 13:01:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ with st.spinner('Updating Report...'):
 
     if Customer:
         Selected_Customer = all_data.loc[all_data['SK_ID_CURR'] == Customer]
-        st.write(Selected_Customer)
-        local(Selected_Customer)
+        st.write(Customer)
+        local(Customer)
         #requests.post("https://c038644.herokuapp.com/local", Selected_Customer)
         #Selected_Customer.to_csv("files/selection.csv")
         #local = requests.get("https://c038644.herokuapp.com/local").json()

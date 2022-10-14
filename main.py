9ed85d75865e65667dc451fb04a7e058e1609b47
c038644 <106696284+c038644@users.noreply.github.com>
2022-10-14 09:53:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ with st.spinner('Updating Report...'):
         #local(Selected_Customer)
         #requests.get("https://c038644.herokuapp.com/local", Selected_Customer)
         #Selected_Customer.to_csv("files/selection.csv")
-        local = requests.get("https://c038644.herokuapp.com/local").json()
-        local_graph_df = pd.DataFrame.from_dict(local)
-        local_graph_df
+        #local = requests.get("https://c038644.herokuapp.com/local").json()
+        #local_graph_df = pd.DataFrame.from_dict(local)
+        #local_graph_df
    
    
     #local = requests.get("https://c038644.herokuapp.com/local").json()
@@ -84,6 +84,8 @@ with st.spinner('Updating Report...'):
     global_graph = requests.get("https://c038644.herokuapp.com/global_data").json()
 
     global_graph_df = pd.DataFrame.from_dict(global_graph)
+    
+    global_graph_df
     
     fig = px.bar(global_graph_df, x = 'Feature', y='Importance')
     

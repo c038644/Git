5154b3a36feb7e067266ef2e294f2cf195cd875c
c038644 <106696284+c038644@users.noreply.github.com>
2022-10-14 10:46:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,76 @@ t1, t2 = st.columns((0.07,1))
 t2.title("Credit Rating Calculator")
 t2.markdown("with Global and Local Customer Data")
 
+def local(Selected_Customer):
 
+  #Local Features Case for a chosen Selected Customer
+  test_df = pd.read_csv("files/P7_test_df.csv")
+
+  #Selected_Customer = pd.read_csv("files/selection.csv")
+
+  print('files loaded')
+
+  #Selected_Customer = Selected_Customer.drop(columns=['Unnamed: 0'])
+  test_df = test_df.drop(columns=['Unnamed: 0'])
+
+  print(Selected_Customer.shape)
+  print(test_df.shape)
+
+  feature_list = list(test_df.columns)
+
+  X = test_df.drop(['TARGET'], axis=1).values
+  y = test_df['TARGET'].values
+
+  data = Selected_Customer.drop(['TARGET'], axis=1).values
+
+  filename = 'files/final_model.sav'
+  loaded_model = pickle.load(open(filename, 'rb'))
+  
+  print('RFC')
+
+  score = loaded_model.fit(X, y).predict(data)
+
+  Credit_given_test = np.max(loaded_model.predict_proba(data))
+
+  if score==0:
+    credit_score=Credit_given_test
+
+  else:
+    credit_score=(1-Credit_given_test)
+
+  print('Get importances')
+
+  # Get numerical feature importances
+  importances = list(loaded_model.feature_importances_)
+
+  # List of tuples with variable and importance
+  feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
+
+  # Sort the feature importances by most important first
+  feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+
+  #Ten most important features
+  ten_most_important = feature_importances[0:10]
+
+  ten_most_important_df = pd.DataFrame(ten_most_important)
+
+  ten_most_important_df.columns = ['Feature', 'Importance']
+
+  ten_most_important_df['Credit Score'] = credit_score
+
+  ten_most_important_df['Credit Granted?'] = None
+
+  if credit_score>=0.35:
+    ten_most_important_df['Credit Granted?'] = ten_most_important_df['Credit Granted?'].fillna('Yes')
+  elif credit_score>=0.25:
+    ten_most_important_df['Credit Granted?'] = ten_most_important_df['Credit Granted?'].fillna('Risky')
+  else:
+    ten_most_important_df['Credit Granted?'] = ten_most_important_df['Credit Granted?'].fillna('No')
+
+  #ten_most_important_df.to_csv("files/Customer_score.csv")
+
+  
+  return(ten_most_important_df)
 
 with st.spinner('Updating Report...'):
     
@@ -35,7 +104,7 @@ with st.spinner('Updating Report...'):
     if Customer:
         Selected_Customer = all_data.loc[all_data['SK_ID_CURR'] == Customer]
         st.write(Selected_Customer)
-        #local(Selected_Customer)
+        local(Selected_Customer)
         #response = requests.post("https://c038644.herokuapp.com/customer").json()
         #print(response.json())
         #data_table1 = pd.DataFrame(response.json())
@@ -54,7 +123,7 @@ with st.spinner('Updating Report...'):
     
     g1, g2, g3 = st.columns((1,1,1))
 
-    local_graph_df = pd.read_csv("files/Customer_score.csv")
+    local_graph_df = ten_most_important_df #pd.read_csv("files/Customer_score.csv")
     
     fig = px.bar(local_graph_df, x = 'Feature', y='Importance')
   

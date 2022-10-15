396e404c0edd2ff540859f5b525243b483ee0b25
c038644 <106696284+c038644@users.noreply.github.com>
2022-10-15 10:13:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -103,9 +103,9 @@ with st.spinner('Updating Report...'):
     #Customer = st.selectbox('Select Customer', Customer_ID, help = 'Filter report to show only one customer')
     Customer = st.multiselect('Select Customer', Customer_ID, help = 'Filter report to show only one customer')
     Customer
-    #lst = [Customer]
-    Customer = Customer[2:]
-    Customer
+    res = [sub[2 : ] for sub in Customer]
+    #Customer = Customer[2:]
+    res
     #selector = st.multiselect("Select WELL:", labels)
     
     

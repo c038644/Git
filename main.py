3f1737d7aed749182c52ca7a97c17cd7dddc2aae
c038644 <106696284+c038644@users.noreply.github.com>
2022-10-15 11:04:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,11 @@ with st.spinner('Updating Report...'):
     if Customer:
         Selected_Customer = all_data.loc[all_data['SK_ID_CURR'] == Customer]
         st.write(Selected_Customer)
-        local_graph_df = local(Selected_Customer, all_data)
+       data = pd.read_json(
+       requests.get("https://c038644.herokuapp.com/customer", params={"selector": Customer}).json()
+       )
+       data
+        #local_graph_df = local(Selected_Customer, all_data)
    
     #local = requests.get("https://c038644.herokuapp.com/local").json()
     #local_graph_df = pd.DataFrame.from_dict(local)

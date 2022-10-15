5ca76fd5286cee71e6a809b4a94d4eb45b022e10
c038644 <106696284+c038644@users.noreply.github.com>
2022-10-15 14:15:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -95,6 +95,10 @@ with st.spinner('Updating Report...'):
     Customer_ID = requests.get("https://c038644.herokuapp.com/labels").json()
 
     Customer_df = pd.DataFrame.from_dict(Customer_ID)
+    
+    Customer_df = str(Customer_df)
+    
+    print(type(Customer_df))
 
     selector = st.selectbox('Select Customer', Customer_df, help = 'Filter report to show only one customer')
     

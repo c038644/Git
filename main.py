3ccc2340e31df1b1074752f0971512aca0e91a3f
c038644 <106696284+c038644@users.noreply.github.com>
2022-12-05 11:32:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import requests
 import json
-import pickle
 
 st.set_page_config(page_title='Credit Rating Calculator',  layout='wide', page_icon=':Calculator:')
 
@@ -21,36 +20,40 @@ t1, t2 = st.columns((0.07,1))
 t2.title("Credit Rating Calculator")
 t2.markdown("with Global and Local Customer Data")
 
+
 with st.spinner('Updating Report...'):
+    
+    Customer_ID = pd.read_csv("files/Customer_ID.csv")
+    Customer_ID = Customer_ID.drop(columns=['Unnamed: 0'])
     
     all_data = pd.read_csv("files/P7_test_df.csv")
     all_data = all_data.drop(columns=['Unnamed: 0'])
 
-    Customer_ID = requests.get("https://c038644.herokuapp.com/labels").json()
+    Customer = st.selectbox('Select Customer', Customer_ID, help = 'Filter report to show only one customer')
 
-    Customer_df = pd.DataFrame.from_dict(Customer_ID)
-    
-    selector = st.selectbox('Select Customer', Customer_df, help = 'Filter report to show only one customer')
-    
-    #if Customer:
-    Selected_Customer = all_data.loc[all_data['SK_ID_CURR'] == selector]
-    st.write(Selected_Customer)
-    data = requests.get("https://c038644.herokuapp.com/customer", params={"selector": selector}).json()
-   
-    local = requests.get("https://c038644.herokuapp.com/local").json()
-    local_graph_df = pd.DataFrame.from_dict(local)
-    
+
+    if Customer:
+        Selected_Customer = all_data.loc[all_data['SK_ID_CURR'] == Customer]
+        st.write(Selected_Customer)
+        Selected_Customer.to_csv("files/selection.csv")
+        local = requests.get("local").json()
+        #st.json(local) 
+       
+
     g1, g2, g3 = st.columns((1,1,1))
 
+    local_graph_df = pd.read_csv("files/Customer_score.csv")
+    
     fig = px.bar(local_graph_df, x = 'Feature', y='Importance')
-  
+    
+ 
     fig.update_layout(title_text="Local Features Graph",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
     
     g1.plotly_chart(fig, use_container_width=True)
 
     fig2 = go.Figure(go.Indicator(
         mode = "gauge+number+delta",
-        value = local_graph_df.iat[0,1],
+        value = local_graph_df.iat[0,3],
         domain = {'x': [0, 1], 'y': [0, 1]},
         title = {'text': "Credit Rating", 'font': {'size': 24}},
         gauge = {
@@ -72,15 +75,21 @@ with st.spinner('Updating Report...'):
 
     g2.plotly_chart(fig2, use_container_width=True) 
 
-    global_graph = requests.get("https://c038644.herokuapp.com/global_data").json()
+    dashboard = requests.get("global_data").json()
 
-    global_graph_df = pd.DataFrame.from_dict(global_graph)
+    #global_data() 
+
+    global_graph_df = pd.read_csv("files/Global_Features.csv")
+
+    global_graph_df = global_graph_df.drop(columns=['Unnamed: 0'])
     
     fig = px.bar(global_graph_df, x = 'Feature', y='Importance')
     
     fig.update_layout(title_text="Global Features Graph",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
     
     g3.plotly_chart(fig, use_container_width=True)
+
+    Selected_Customer = pd.read_csv("files/selection.csv")
 
     Feature_List = pd.read_csv("files/P7_Features.csv")
 

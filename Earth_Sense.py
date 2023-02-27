255624e7e60dba834fa1b0c26cd37dfd9feab056
c038644 <106696284+c038644@users.noreply.github.com>
2023-02-27 15:14:19 +0100

    Update Earth_Sense.py

--- Earth_Sense.py
+++ Earth_Sense.py
@@ -33,8 +33,7 @@ with st.spinner('Updating Report...'):
 
     g1, g2= st.columns((10, 1))
     
-    df = df.set_index('95-NO2(ug/m3)-slotA')
-    
+     
     #fig = px.line(df, x = 'Time', y=Feature)
     fig = px.line(df, x = 'Time', y=['95-NO2(ug/m3)-slotA', '95-NO2(ug/m3)-slotB'])#, 'Nitrogen dioxide'])
      

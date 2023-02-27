51bea0b10461d4dcd35382d2da1366abb7dde288
c038644 <106696284+c038644@users.noreply.github.com>
2023-02-27 10:14:50 +0100

    Update Earth_Sense.py

--- Earth_Sense.py
+++ Earth_Sense.py
@@ -63,7 +63,7 @@ with st.spinner('Updating Report...'):
     #fig = px.line(graph_df, x = 'Time', y='Ozone')
     df = px.data.gapminder().query("Day==Date")
     
-    fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+    fig = px.line(df, x = 'Time', y='Ozone', title='Life expectancy in Canada')
     #fig.update_layout(title_text="Local Features Graph",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
     
     g1.plotly_chart(fig, use_container_width=True)

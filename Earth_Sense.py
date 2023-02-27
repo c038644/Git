cf332d7931ef4f4d491ee5f9916853ca97f076fd
c038644 <106696284+c038644@users.noreply.github.com>
2023-02-27 09:18:36 +0100

    Update Earth_Sense.py

--- Earth_Sense.py
+++ Earth_Sense.py
@@ -61,7 +61,7 @@ with st.spinner('Updating Report...'):
     fig = px.line(df, x = 'Time', y='Ozone')
     
  
-    fig.update_layout(title_text="Local Features Graph",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
+    #fig.update_layout(title_text="Local Features Graph",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
     
-    g1.plotly_chart(fig, use_container_width=True)
+    #g1.plotly_chart(fig, use_container_width=True)
 

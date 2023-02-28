e6efbf47f10f6600d81be3b91c5b0cd72be44bf5
c038644 <106696284+c038644@users.noreply.github.com>
2023-02-28 10:05:25 +0100

    Update Earth_Sense.py

--- Earth_Sense.py
+++ Earth_Sense.py
@@ -39,7 +39,7 @@ with st.spinner('Updating Report...'):
     
     #Selected_Feature = Feature
     
-    if (Feature == Feature_List[1]):
+    if (Feature == Feature_List[0][1]):
      #F1, F2, F3 = '95-NO2(ug/m3)-slotA', '95-NO2(ug/m3)-slotB', 'Nitrogen dioxide'
      print("hello")
  

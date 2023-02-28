5dde776db90f88cdc8a82b08f014592c9f784540
c038644 <106696284+c038644@users.noreply.github.com>
2023-02-28 14:24:39 +0100

    Update Earth_Sense.py

--- Earth_Sense.py
+++ Earth_Sense.py
@@ -62,7 +62,7 @@ with st.spinner('Updating Report...'):
 
     g3, g4, g5, g6, g7, g8= st.columns((1, 1, 1, 1, 1, 1))
     
-    st.dataframe(data=CombinedAirQualityData[F1], width=50, use_container_width=False)
+    st.dataframe(data=CombinedAirQualityData[F1], width=200, use_container_width=True)
     CombinedAirQualityData[F2]
     CombinedAirQualityData[F3]
     

7d1854c4de54d92152d8c8ce8d5e436368bd9407
c038644 <106696284+c038644@users.noreply.github.com>
2023-02-28 13:30:01 +0100

    Update Earth_Sense.py

--- Earth_Sense.py
+++ Earth_Sense.py
@@ -63,9 +63,9 @@ with st.spinner('Updating Report...'):
     g3, g4= st.columns((100, 1))
     
     #df['Difference'] = ((df[F3] - ((df[F2] + df[F1])/2))/(df[F3] + df[F2] + df[F1])) * 100
-    CombinedAirQualityData['Difference'] = (dfCombinedAirQualityData[F3] - (CombinedAirQualityData[F2] + CombinedAirQualityData[F1])/2)
+    CombinedAirQualityData['Difference'] = (CombinedAirQualityData[F3] - (CombinedAirQualityData[F2] + CombinedAirQualityData[F1])/2)
     
-    fig2 = px.line(df, x = 'Time', y='Difference')
+    fig2 = px.line(CombinedAirQualityData, x = 'Time', y='Difference')
      
     g3.plotly_chart(fig2, use_container_width=True)
  

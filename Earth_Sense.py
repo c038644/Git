1cd9f8bf668196ac16035a7f156b4cdc5f4a9269
c038644 <106696284+c038644@users.noreply.github.com>
2023-02-28 13:41:13 +0100

    Update Earth_Sense.py

--- Earth_Sense.py
+++ Earth_Sense.py
@@ -65,7 +65,7 @@ with st.spinner('Updating Report...'):
     #df['Difference'] = ((df[F3] - ((df[F2] + df[F1])/2))/(df[F3] + df[F2] + df[F1])) * 100
     CombinedAirQualityData['Difference'] = (sum(CombinedAirQualityData[F3] - (CombinedAirQualityData[F2] + CombinedAirQualityData[F1])/2))
     CombinedAirQualityData['Difference']
-    CombinedAirQualityData['Percentage'] = CombinedAirQualityData['Difference'] / sum(CombinedAirQualityData['Difference'])
+    CombinedAirQualityData['Percentage'] = CombinedAirQualityData['Difference'] / sum(CombinedAirQualityData[F3] + CombinedAirQualityData[F2] + CombinedAirQualityData[F1])
     CombinedAirQualityData['Percentage']
     
     #fig2 = px.line(CombinedAirQualityData, x = 'Time', y='Difference')

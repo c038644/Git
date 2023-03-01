25741427eb95cb616846f058644f6b67fb324f90
c038644 <106696284+c038644@users.noreply.github.com>
2023-03-01 12:02:08 +0100

    Update Earth_Sense.py

--- Earth_Sense.py
+++ Earth_Sense.py
@@ -72,19 +72,19 @@ with st.spinner('Updating Report...'):
      
     g11.plotly_chart(fig2, use_container_width=True)  
     
-    g3, g4, g5, g6, g7, g8= st.columns((1, 1, 1, 1, 1, 1))
+    g6, g7, g8= st.columns((2, 2, 1))
     
-    with g3:
-     st.dataframe(data=CombinedAirQualityData[F1], use_container_width=True)
-    with g4:
-     st.dataframe(data=CombinedAirQualityData[F2], use_container_width=True) 
-    with g5:
-     st.dataframe(data=CombinedAirQualityData[F3], use_container_width=True)  
+    #with g3:
+    # st.dataframe(data=CombinedAirQualityData[F1], use_container_width=True)
+    #with g4:
+    # st.dataframe(data=CombinedAirQualityData[F2], use_container_width=True) 
+    #with g5:
+    # st.dataframe(data=CombinedAirQualityData[F3], use_container_width=True)  
      
     with g6:
-     st.dataframe(data=CombinedAirQualityData[['Day', 'Difference']], use_container_width=True)  
+     st.dataframe(data=CombinedAirQualityData[['Day', 'Time', 'Difference']], use_container_width=True)  
     with g7:
-     st.dataframe(data=CombinedAirQualityData['Percentage'], use_container_width=True)
+     st.dataframe(data=CombinedAirQualityData[['Day', 'Time', 'Percentage']], use_container_width=True)
     
     with g8:
      st.write("Total difference") 

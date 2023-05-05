c789544536a39a691d543e884e8e07a253999581
c038644 <106696284+c038644@users.noreply.github.com>
2023-05-05 04:24:30 +0100

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,1 +1,12 @@
-test
+import requests
+import random
+import time
+import random
+import streamlit as st
+
+#this is the header
+ 
+t1, t2 = st.columns((0.07,1)) 
+
+t2.title("Pokemon Game")
+t2.markdown("version 1.0")

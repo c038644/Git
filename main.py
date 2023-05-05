1f927a33583c7ba4adbc03d07e87cac8c30679e2
c038644 <106696284+c038644@users.noreply.github.com>
2023-05-05 04:03:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,225 +1,248 @@
 ##########################################################
 # to run: streamlit run main.py
 ##########################################################
-import pandas as pd
-import numpy as np
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 import requests
-import json
-from sklearn.model_selection import train_test_split
-from imblearn.under_sampling import RandomUnderSampler
-from sklearn.ensemble import RandomForestClassifier
+import random
+import time
+import random
+import streamlit as st
 
-def local():
+def get_pokemon_details(pokemon):
+    """Retrieves details about a Pokemon from the PokeAPI."""
+    pokemon_data = requests.get(pokemon['pokemon_species']['url']).json()
+    pokemon_name = pokemon_data['name']
+    pokemon_url = pokemon_data['varieties'][0]['pokemon']['url']
+    pokemon_details = requests.get(pokemon_url).json()
+    pokemon_type = pokemon_details['types'][0]['type']['name']
+    pokemon_ability = pokemon_details['abilities'][0]['ability']['name']
+    return pokemon_name, pokemon_type, pokemon_ability
 
-  #Local Features Case for a chosen Selected Customer
-  test_df = pd.read_csv("files/P7_test_df.csv")
+def get_pokemon_moves(pokemon_name):
+    url = f"https://pokeapi.co/api/v2/"
+    pokemon_url = f"{url}pokemon/{pokemon_name}"
+    response = requests.get(pokemon_url)
+    if response.ok:
+        moves = [move["move"]["name"] for move in response.json()["moves"]]
+        return moves
+    else:
+        return None
 
-  Selected_Customer = pd.read_csv("files/selection.csv")
+def get_pokemon_attributes(pokemon_name):
+    base_url = "https://pokeapi.co/api/v2/"
+    pokemon_url = f"{base_url}pokemon/{pokemon_name}"
+    response = requests.get(pokemon_url)
 
-  Selected_Customer = Selected_Customer.drop(columns=['Unnamed: 0'])
-  test_df = test_df.drop(columns=['Unnamed: 0'])
+    if response.status_code == 200:
+        pokemon_details = response.json()
+        attributes = {}
 
-  feature_list = list(test_df.columns)
+        for stat in pokemon_details['stats']:
+            stat_name = stat['stat']['name']
+            stat_value = stat['base_stat']
+            attributes[stat_name] = stat_value
 
-  X = test_df.drop(['TARGET'], axis=1).values
-  y = test_df['TARGET'].values
-
-  data = Selected_Customer.drop(['TARGET'], axis=1).values
-
-  X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-  undersample = RandomUnderSampler(sampling_strategy=1)
-
-  X_train, y_train = undersample.fit_resample(X_train, y_train)
-
-  rfc = RandomForestClassifier(max_depth=13, min_samples_leaf=2, min_samples_split=8, n_estimators=552)
-  
-  score = rfc.fit(X_train, y_train).predict(data)
-
-  Credit_given_test = np.max(rfc.predict_proba(data))
-
-  if score==0:
-    credit_score=Credit_given_test
-
-  else:
-    credit_score=(1-Credit_given_test)
-
-   # Get numerical feature importances
-  importances = list(rfc.feature_importances_)
-
-  # List of tuples with variable and importance
-  feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
-
-  # Sort the feature importances by most important first
-  feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
-
-  #Ten most important features
-  ten_most_important = feature_importances[0:10]
-
-  ten_most_important_df = pd.DataFrame(ten_most_important)
-
-  ten_most_important_df.columns = ['Feature', 'Importance']
-
-  ten_most_important_df['Credit Score'] = credit_score
-
-  ten_most_important_df['Credit Granted?'] = None
-
-  if credit_score>=0.35:
-    ten_most_important_df['Credit Granted?'] = ten_most_important_df['Credit Granted?'].fillna('Yes')
-  elif credit_score>=0.25:
-    ten_most_important_df['Credit Granted?'] = ten_most_important_df['Credit Granted?'].fillna('Risky')
-  else:
-    ten_most_important_df['Credit Granted?'] = ten_most_important_df['Credit Granted?'].fillna('No')
-
-  ten_most_important_df.to_csv("files/Customer_score.csv")
-
-  print('Customer Score Ready')
-  return(ten_most_important_df)
-
-
-def global_data():
-
-  test_df = pd.read_csv("files/P7_test_df.csv")
-
-  #Global Features Case
-
-  feature_list = list(test_df.columns)
-
-  X = test_df.drop(['TARGET'], axis=1).values
-  y = test_df['TARGET'].values
-
-  X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-  undersample = RandomUnderSampler(sampling_strategy=1)
-
-  X_train, y_train = undersample.fit_resample(X_train, y_train)
-
-  rfc = RandomForestClassifier(max_depth=13, min_samples_leaf=2, min_samples_split=8, n_estimators=552)
-
-  rfc.fit(X_train, y_train)
-
-  # Get numerical feature importances
-  importances = list(rfc.feature_importances_)
-
-  # List of tuples with variable and importance
-  feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
-
-  # Sort the feature importances by most important first
-  feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
-
-  #Ten most important features
-  ten_most_important = feature_importances[0:10]
-
-  Global_Features = pd.DataFrame(ten_most_important)
-
-  Global_Features.columns = ['Feature', 'Importance']
-
-  Global_Features.to_csv("files/Global_Features.csv")
-
-  # Print out the feature and importances 
-  return(Global_Features)
-
-st.set_page_config(page_title='Credit Rating Calculator',  layout='wide', page_icon=':Calculator:')
+        return attributes
+    else:
+        print(f"Error: Failed to retrieve data for {pokemon_name}")
+        return None    
 
 #this is the header
  
 t1, t2 = st.columns((0.07,1)) 
 
-t2.title("Credit Rating Calculator")
-t2.markdown("with Global and Local Customer Data")
+t2.title("Pokemon Game")
+t2.markdown("version 1.0")
+
+print("Welcome to the Pokemon game!")
+game_mode = input("Enter '1' for single player or '2' for two player: ")
+
+if game_mode == '1':
+    # Single player version of the game
+
+    # Make a GET request to the PokeAPI to retrieve information about the Kanto Pokedex
+    response = requests.get('https://pokeapi.co/api/v2/pokedex/kanto')
+
+    # Parse the JSON response into a Python dictionary
+    pokedex = response.json()
+
+    # Retrieve the list of Pokemon in the Kanto Pokedex
+    kanto_pokemon = pokedex['pokemon_entries']
+
+    # Randomly select 20 Pokemon from the Kanto Pokedex
+    selected_pokemon = random.sample(kanto_pokemon, 20)
+
+    # Print information about the selected Pokemon
+    print("Here are 20 random Pokemon from the Kanto Pokedex:")
+    for pokemon in selected_pokemon:
+        pokemon_name, pokemon_type, pokemon_ability = get_pokemon_details(pokemon)
+        print(f"{pokemon_name:<15} (Type: {pokemon_type:<10}, Ability: {pokemon_ability:<20})")
+
+    # Prompt the user to select a Pokemon
+    while True:
+        print("\nChoose 1 and begin your journey!")
+        pokemon_choice = input("Enter the name of the Pokemon you want to choose: ")
+
+        # Find the selected Pokemon in the list of randomly selected Pokemon
+        selected_pokemon_names = [get_pokemon_details(pokemon)[0] for pokemon in selected_pokemon]
+        if pokemon_choice in selected_pokemon_names:
+            print(f"You have chosen {pokemon_choice}. Let us begin!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+    print("\nGame starting...")
+
+elif game_mode == '2':
+    # Two player version of the game
+
+    # Get the names of the players
+    player1_name = input("Enter the name of player 1: ")
+    player2_name = input("Enter the name of player 2: ")
+
+    # Make a GET request to the PokeAPI to retrieve information about the Kanto Pokedex
+    response = requests.get('https://pokeapi.co/api/v2/pokedex/kanto')
+
+    # Parse the JSON response into a Python dictionary
+    pokedex = response.json()
+
+    # Retrieve the list of Pokemon in the Kanto Pokedex
+    kanto_pokemon = pokedex['pokemon_entries']
+
+    # Randomly select 20 Pokemon from the Kanto Pokedex
+    selected_pokemon = random.sample(kanto_pokemon, 20)
+
+    # Print information about the selected Pokemon
+    print("Here are 20 random Pokemon from the Kanto Pokedex:")
+    for pokemon in selected_pokemon:
+        pokemon_name, pokemon_type, pokemon_ability = get_pokemon_details(pokemon)
+        print(f"{pokemon_name:<15} (Type: {pokemon_type:<10}, Ability: {pokemon_ability:<20})")
+
+    # Prompt player 1 to select a Pokemon
+    while True:
+        print(f"\n{player1_name}, choose 1 and begin your journey!")
+        player1_choice = input("Enter the name of the Pokemon you want to choose: ")
+
+    # Find the selected Pokemon in the list of randomly selected Pokemon
+        selected_pokemon_names = [get_pokemon_details(pokemon)[0] for pokemon in selected_pokemon]
+        if player1_choice not in selected_pokemon_names:
+            print("Invalid choice. Please try again.")
+            continue
+
+    # Prompt player 2 to select a Pokemon
+        print(f"\n{player2_name}, choose 1 and begin your journey!")
+        player2_choice = input("Enter the name of the Pokemon you want to choose: ")
+
+    # Find the selected Pokemon in the list of randomly selected Pokemon
+        if player2_choice not in selected_pokemon_names or player2_choice == player1_choice:
+            print("Invalid choice. Please try again.")
+            continue
+
+    # Game starting
+        print("\nGame starting...")
+        print(f"{player1_name} has chosen {player1_choice}.")
+        print(f"{player2_name} has chosen {player2_choice}.")
+        print("Let the battle begin!")
+    # Code for the battle between player 1's and player 2's Pokemon
+    # ...
+        break
+# Find the selected Pokemon in the list of randomly selected Pokemon
+selected_pokemon_names = [get_pokemon_details(pokemon)[0] for pokemon in selected_pokemon]
+
+if player1_choice in selected_pokemon_names:
+    attributes = get_pokemon_attributes(player1_choice)
+    if attributes:
+        moves = get_pokemon_moves(player1_choice)
+        print("\n" + "-"*40)
+        print(f"{player1_choice.upper()} BASE STATS:")
+        for stat_name, stat_value in attributes.items():
+            print(f"{stat_name.capitalize()}: {stat_value}")
+        print("-"*40)
+    else:
+        print(f"Error: Failed to retrieve attributes for {player1_choice}. Please try again.")
+
+if player2_choice in selected_pokemon_names:
+    attributes = get_pokemon_attributes(player2_choice)
+    if attributes:
+        moves = get_pokemon_moves(player2_choice)
+        print("\n" + "-"*40)
+        print(f"{player2_choice.upper()} BASE STATS:")
+        for stat_name, stat_value in attributes.items():
+            print(f"{stat_name.capitalize()}: {stat_value}")
+        print("-"*40)
+    else:
+        print(f"Error: Failed to retrieve attributes for {player2_choice}. Please try again.")
 
 
-with st.spinner('Updating Report...'):
-    
-    Customer_ID = pd.read_csv("files/Customer_ID.csv")
-    Customer_ID = Customer_ID.drop(columns=['Unnamed: 0'])
-    
-    all_data = pd.read_csv("files/P7_test_df.csv")
-    all_data = all_data.drop(columns=['Unnamed: 0'])
 
-    Customer = st.selectbox('Select Customer', Customer_ID, help = 'Filter report to show only one customer')
+# Find the selected Pokemon in the list of randomly selected Pokemon
+selected_pokemon_names = [get_pokemon_details(pokemon)[0] for pokemon in selected_pokemon]
 
+if player1_choice in selected_pokemon_names and player2_choice in selected_pokemon_names:
+    player1_stats = get_pokemon_attributes(player1_choice)
+    player2_stats = get_pokemon_attributes(player2_choice)
 
-    if Customer:
-        Selected_Customer = all_data.loc[all_data['SK_ID_CURR'] == Customer]
-        st.write(Selected_Customer)
-        Selected_Customer.to_csv("files/selection.csv")
-        #local = requests.get("local").json()
-        #st.json(local) 
-    
-    local()
+    # Compare the total base stats of each player's selected Pokemon
+    player1_total_stats = sum(player1_stats.values())
+    player2_total_stats = sum(player2_stats.values())
 
-    g1, g2, g3 = st.columns((1,1,1))
+    print(f"Player 1: {player1_choice.upper()}")
+    for stat_name, stat_value in player1_stats.items():
+        print(f"{stat_name.capitalize()}: {stat_value}")
+    print(f"Total base stats: {player1_total_stats}\n")
 
-    local_graph_df = pd.read_csv("files/Customer_score.csv")
-    
-    fig = px.bar(local_graph_df, x = 'Feature', y='Importance')
-    
- 
-    fig.update_layout(title_text="Local Features Graph",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
-    
-    g1.plotly_chart(fig, use_container_width=True)
+    print(f"Player 2: {player2_choice.upper()}")
+    for stat_name, stat_value in player2_stats.items():
+        print(f"{stat_name.capitalize()}: {stat_value}")
+    print(f"Total base stats: {player2_total_stats}\n")
 
-    fig2 = go.Figure(go.Indicator(
-        mode = "gauge+number+delta",
-        value = local_graph_df.iat[0,3],
-        domain = {'x': [0, 1], 'y': [0, 1]},
-        title = {'text': "Credit Rating", 'font': {'size': 24}},
-        gauge = {
-            'axis': {'range': [0, 1], 'tickwidth': 1, 'tickcolor': "darkblue"},
-            'bar': {'color': "black"},
-            'bgcolor': "white",
-            'borderwidth': 2,
-            'bordercolor': "gray",
-            'steps': [
-                {'range': [0, 0.26], 'color': 'red'},
-                {'range': [0.26, 0.36], 'color': 'orange'},
-                {'range': [0.36, 1], 'color': 'green'}],
-            'threshold': {
-                'line': {'color': "blue", 'width': 4},
-                'thickness': 0.75,
-                'value': 0.31}}))
+    print("Let the battle begin!\n")
+    time.sleep(1)
 
-    fig2.update_layout(paper_bgcolor = "lavender", font = {'color': "darkblue", 'family': "Arial"})
+    # Determine which player attacks first
+    players = [(player1_choice, player1_total_stats), (player2_choice, player2_total_stats)]
+    random.shuffle(players)
+    first_player = players[0][0]
+    second_player = players[1][0]
 
-    g2.plotly_chart(fig2, use_container_width=True) 
+    print(f"{first_player.upper()} attacks first!\n")
+    time.sleep(1)
 
-    #dashboard = requests.get("global_data").json()
+    while player1_total_stats > 0 and player2_total_stats > 0:
+        # Determine the attack strength of each player
+        player1_attack = random.randint(1, player1_total_stats)
+        player2_attack = random.randint(1, player2_total_stats)
 
-    #global_data() 
+        # Reduce the total base stats of the player that attacks
+        if first_player == player1_choice:
+            player2_total_stats -= player1_attack
+            print(f"{first_player.upper()} attacks with {player1_attack} strength!")
+            time.sleep(1)
+            if player2_total_stats <= 0:
+                break
+            player1_total_stats -= player2_attack
+            print(f"{second_player.upper()} counterattacks with {player2_attack} strength!")
+            time.sleep(1)
+            if player1_total_stats <= 0:
+                break
+        else:
+            player1_total_stats -= player2_attack
+            print(f"{first_player.upper()} attacks with {player2_attack} strength!")
+            time.sleep(1)
+            if player1_total_stats <= 0:
+                break
+            player2_total_stats -= player1_attack
+            print(f"{second_player.upper()} counterattacks with {player1_attack} strength!")
+            time.sleep(1)
+            if player2_total_stats <= 0:
+                break
 
-    global_graph_df = pd.read_csv("files/Global_Features.csv")
+    print("\nThe battle is over!\n")
+    time.sleep(1)
 
-    global_graph_df = global_graph_df.drop(columns=['Unnamed: 0'])
-    
-    fig = px.bar(global_graph_df, x = 'Feature', y='Importance')
-    
-    fig.update_layout(title_text="Global Features Graph",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
-    
-    g3.plotly_chart(fig, use_container_width=True)
-
-    Selected_Customer = pd.read_csv("files/selection.csv")
-
-    Feature_List = pd.read_csv("files/P7_Features.csv")
-
-    Feature = st.selectbox('Select Feature', Feature_List, help = 'Filter report to show only one feature')
-
-    Selected_Feature = all_data.loc[all_data[Feature] == Feature].any()
-
-    g4, g5 = st.columns((1,2))
-
-    fig = px.scatter(Selected_Customer, x = 'SK_ID_CURR', y = Feature)
-    
-    fig.update_layout(title_text="Local Feature Graph",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
-    
-    g4.plotly_chart(fig, use_container_width=True)
-
-    fig = px.scatter(all_data, x = 'SK_ID_CURR', y = Feature)
-    
-    fig.update_layout(title_text="Global Feature Graph",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
-    
-    g5.plotly_chart(fig, use_container_width=True)
+    if player1_total_stats > player2_total_stats:
+        print(f"{first_player.upper()} wins!")
+    elif player2_total_stats > player1_total_stats:
+        print(f"{second_player.upper()} wins!")
+    else:
+        print("It's a tie!")
+else:
+    print("Invalid Pokemon choice for one or both players. Please try again.") 

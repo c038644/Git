c36f04c7508fa18bc888d447b2df42cf62ec6597
c038644 <106696284+c038644@users.noreply.github.com>
2023-05-05 06:50:57 +0100

    Update Earth_Sense.py

--- Earth_Sense.py
+++ Earth_Sense.py
@@ -3,6 +3,8 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
+import time
+import random
 
 st.set_page_config(page_title='Air Quality Analysis',  layout='wide', page_icon=':Calculator:')
 
@@ -14,85 +16,240 @@ t2.title("Air Quality Analysis")
 t2.markdown("with Zephyr and AURN Data")
 
 
-with st.spinner('Updating Report...'):
-    
-    Date = pd.read_csv("Day.csv")
-    Week_data_95 = pd.read_csv("Week_data_95.csv")
-    Feature_List = pd.read_csv("Feature_List.csv")
-    CombinedAirQualityData = pd.read_csv("CombinedAirQualityData.csv")
-    AirQualityDataHourly = pd.read_csv("AirQualityDataHourly.csv")
+import requests
+import random
 
-    Day = st.selectbox('Select Day', Date, help = 'Filter report to show only one day')
-    Selected_Day = Week_data_95.loc[Week_data_95['Day'] == Day]
-    Selected_Day_gov = AirQualityDataHourly.loc[AirQualityDataHourly['Day'] == Day]
-    
-    df = pd.merge(Selected_Day, Selected_Day_gov, how="left", on="Time")
-    
-    Feature = st.selectbox('Select Pollutant', Feature_List, help = 'Filter report to show only one pollutant')
-    #Selected_Feature = df.loc[df[Feature] == Feature].any()
+def get_pokemon_details(pokemon):
+    """Retrieves details about a Pokemon from the PokeAPI."""
+    pokemon_data = requests.get(pokemon['pokemon_species']['url']).json()
+    pokemon_name = pokemon_data['name']
+    pokemon_url = pokemon_data['varieties'][0]['pokemon']['url']
+    pokemon_details = requests.get(pokemon_url).json()
+    pokemon_type = pokemon_details['types'][0]['type']['name']
+    pokemon_ability = pokemon_details['abilities'][0]['ability']['name']
+    return pokemon_name, pokemon_type, pokemon_ability
 
-    if (Feature == 'Nitric oxide'):
-     F1 = '95-NO(ug/m3)-slotA'
-     F2 = '95-NO(ug/m3)-slotB'
-     F3 = 'Nitric oxide'
-    elif (Feature == 'Nitrogen dioxide'):
-     F1 = '95-NO2(ug/m3)-slotA'
-     F2 = '95-NO2(ug/m3)-slotB'
-     F3 = 'Nitrogen dioxide'
-    elif (Feature == 'Ozone'):
-     F1 = '95-O3(ug/m3)-slotA'
-     F2 = '95-O3(ug/m3)-slotB'
-     F3 = 'Ozone' 
-    elif (Feature == 'PM2.5 particulate matter'):
-     F1 = '95-PM2.5(ug/m3)-slotA'
-     F2 = '95-PM2.5(ug/m3)-slotB'
-     F3 = 'PM2.5 particulate matter'
-    elif (Feature == 'PM10 particulate matter'):
-     F1 = '95-PM10(ug/m3)-slotA'
-     F2 = '95-PM10(ug/m3)-slotB'
-     F3 = 'PM10 particulate matter'  
-         
-    g0, g10= st.columns((100, 1))
-    st.write("Hourly data for slot A and slot B with AURN data")
- 
-    fig = px.line(df, x = 'Time', y=[F1, F2, F3])
-     
-    g1, g2= st.columns((100, 1))
-   
-    g1.plotly_chart(fig, use_container_width=True)
-    
-    st.write("Weekly data for the average of slot A and slot B with AURN data") 
-    
-    g11, g21= st.columns((100, 1))
-    
-    CombinedAirQualityData['Difference'] = (CombinedAirQualityData[F3] - (0.5*(CombinedAirQualityData[F2] + CombinedAirQualityData[F1])))
-    CombinedAirQualityData['Percentage'] = 100*(CombinedAirQualityData['Difference']) / (0.5*(CombinedAirQualityData[F1] + CombinedAirQualityData[F2]) + CombinedAirQualityData[F3])
-    
-    fig2 = px.bar(CombinedAirQualityData, x = 'Day', y='Difference')
-     
-    g11.plotly_chart(fig2, use_container_width=True)  
-    
-    g6, g8= st.columns((6, 1))
-    
-    #with g3:
-    # st.dataframe(data=CombinedAirQualityData[F1], use_container_width=True)
-    #with g4:
-    # st.dataframe(data=CombinedAirQualityData[F2], use_container_width=True) 
-    #with g5:
-    # st.dataframe(data=CombinedAirQualityData[F3], use_container_width=True)  
-     
-    with g6:
-     st.dataframe(data=CombinedAirQualityData[['Day', 'Time', 'Difference', 'Percentage']], use_container_width=True)  
-    #with g7:
-    # st.dataframe(data=CombinedAirQualityData[['Day', 'Time', 'Percentage']], use_container_width=True)
-    
-    with g8:
-     st.write("Total difference") 
-     st.write(sum(CombinedAirQualityData['Difference']))
-     st.write("Average difference") 
-     st.write(sum(CombinedAirQualityData['Difference'])/168)
-     st.write("Average Percentage difference")
-     st.write(sum(CombinedAirQualityData['Percentage'])/168)
+def get_pokemon_moves(pokemon_name):
+    url = f"https://pokeapi.co/api/v2/"
+    pokemon_url = f"{url}pokemon/{pokemon_name}"
+    response = requests.get(pokemon_url)
+    if response.ok:
+        moves = [move["move"]["name"] for move in response.json()["moves"]]
+        return moves
+    else:
+        return None
+
+def get_pokemon_attributes(pokemon_name):
+    base_url = "https://pokeapi.co/api/v2/"
+    pokemon_url = f"{base_url}pokemon/{pokemon_name}"
+    response = requests.get(pokemon_url)
+
+    if response.status_code == 200:
+        pokemon_details = response.json()
+        attributes = {}
+
+        for stat in pokemon_details['stats']:
+            stat_name = stat['stat']['name']
+            stat_value = stat['base_stat']
+            attributes[stat_name] = stat_value
+
+        return attributes
+    else:
+        print(f"Error: Failed to retrieve data for {pokemon_name}")
+        return None    
+
+print("Welcome to the Pokemon game!")
+game_mode = input("Enter '1' for single player or '2' for two player: ")
+
+if game_mode == '1':
+    # Single player version of the game
+
+    # Make a GET request to the PokeAPI to retrieve information about the Kanto Pokedex
+    response = requests.get('https://pokeapi.co/api/v2/pokedex/kanto')
+
+    # Parse the JSON response into a Python dictionary
+    pokedex = response.json()
+
+    # Retrieve the list of Pokemon in the Kanto Pokedex
+    kanto_pokemon = pokedex['pokemon_entries']
+
+    # Randomly select 20 Pokemon from the Kanto Pokedex
+    selected_pokemon = random.sample(kanto_pokemon, 20)
+
+    # Print information about the selected Pokemon
+    print("Here are 20 random Pokemon from the Kanto Pokedex:")
+    for pokemon in selected_pokemon:
+        pokemon_name, pokemon_type, pokemon_ability = get_pokemon_details(pokemon)
+        print(f"{pokemon_name:<15} (Type: {pokemon_type:<10}, Ability: {pokemon_ability:<20})")
+
+    # Prompt the user to select a Pokemon
+    while True:
+        print("\nChoose 1 and begin your journey!")
+        pokemon_choice = input("Enter the name of the Pokemon you want to choose: ")
+
+        # Find the selected Pokemon in the list of randomly selected Pokemon
+        selected_pokemon_names = [get_pokemon_details(pokemon)[0] for pokemon in selected_pokemon]
+        if pokemon_choice in selected_pokemon_names:
+            print(f"You have chosen {pokemon_choice}. Let us begin!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+    print("\nGame starting...")
+
+elif game_mode == '2':
+    # Two player version of the game
+
+    # Get the names of the players
+    player1_name = input("Enter the name of player 1: ")
+    player2_name = input("Enter the name of player 2: ")
+
+    # Make a GET request to the PokeAPI to retrieve information about the Kanto Pokedex
+    response = requests.get('https://pokeapi.co/api/v2/pokedex/kanto')
+
+    # Parse the JSON response into a Python dictionary
+    pokedex = response.json()
+
+    # Retrieve the list of Pokemon in the Kanto Pokedex
+    kanto_pokemon = pokedex['pokemon_entries']
+
+    # Randomly select 20 Pokemon from the Kanto Pokedex
+    selected_pokemon = random.sample(kanto_pokemon, 20)
+
+    # Print information about the selected Pokemon
+    print("Here are 20 random Pokemon from the Kanto Pokedex:")
+    for pokemon in selected_pokemon:
+        pokemon_name, pokemon_type, pokemon_ability = get_pokemon_details(pokemon)
+        print(f"{pokemon_name:<15} (Type: {pokemon_type:<10}, Ability: {pokemon_ability:<20})")
+
+    # Prompt player 1 to select a Pokemon
+    while True:
+        print(f"\n{player1_name}, choose 1 and begin your journey!")
+        player1_choice = input("Enter the name of the Pokemon you want to choose: ")
+
+    # Find the selected Pokemon in the list of randomly selected Pokemon
+        selected_pokemon_names = [get_pokemon_details(pokemon)[0] for pokemon in selected_pokemon]
+        if player1_choice not in selected_pokemon_names:
+            print("Invalid choice. Please try again.")
+            continue
+
+    # Prompt player 2 to select a Pokemon
+        print(f"\n{player2_name}, choose 1 and begin your journey!")
+        player2_choice = input("Enter the name of the Pokemon you want to choose: ")
+
+    # Find the selected Pokemon in the list of randomly selected Pokemon
+        if player2_choice not in selected_pokemon_names or player2_choice == player1_choice:
+            print("Invalid choice. Please try again.")
+            continue
+
+    # Game starting
+        print("\nGame starting...")
+        print(f"{player1_name} has chosen {player1_choice}.")
+        print(f"{player2_name} has chosen {player2_choice}.")
+        print("Let the battle begin!")
+    # Code for the battle between player 1's and player 2's Pokemon
+    # ...
+        break
+# Find the selected Pokemon in the list of randomly selected Pokemon
+selected_pokemon_names = [get_pokemon_details(pokemon)[0] for pokemon in selected_pokemon]
+
+if player1_choice in selected_pokemon_names:
+    attributes = get_pokemon_attributes(player1_choice)
+    if attributes:
+        moves = get_pokemon_moves(player1_choice)
+        print("\n" + "-"*40)
+        print(f"{player1_choice.upper()} BASE STATS:")
+        for stat_name, stat_value in attributes.items():
+            print(f"{stat_name.capitalize()}: {stat_value}")
+        print("-"*40)
+    else:
+        print(f"Error: Failed to retrieve attributes for {player1_choice}. Please try again.")
+
+if player2_choice in selected_pokemon_names:
+    attributes = get_pokemon_attributes(player2_choice)
+    if attributes:
+        moves = get_pokemon_moves(player2_choice)
+        print("\n" + "-"*40)
+        print(f"{player2_choice.upper()} BASE STATS:")
+        for stat_name, stat_value in attributes.items():
+            print(f"{stat_name.capitalize()}: {stat_value}")
+        print("-"*40)
+    else:
+        print(f"Error: Failed to retrieve attributes for {player2_choice}. Please try again.")
+
+
+# Find the selected Pokemon in the list of randomly selected Pokemon
+selected_pokemon_names = [get_pokemon_details(pokemon)[0] for pokemon in selected_pokemon]
+
+if player1_choice in selected_pokemon_names and player2_choice in selected_pokemon_names:
+    player1_stats = get_pokemon_attributes(player1_choice)
+    player2_stats = get_pokemon_attributes(player2_choice)
+
+    # Compare the total base stats of each player's selected Pokemon
+    player1_total_stats = sum(player1_stats.values())
+    player2_total_stats = sum(player2_stats.values())
+
+    print(f"Player 1: {player1_choice.upper()}")
+    for stat_name, stat_value in player1_stats.items():
+        print(f"{stat_name.capitalize()}: {stat_value}")
+    print(f"Total base stats: {player1_total_stats}\n")
+
+    print(f"Player 2: {player2_choice.upper()}")
+    for stat_name, stat_value in player2_stats.items():
+        print(f"{stat_name.capitalize()}: {stat_value}")
+    print(f"Total base stats: {player2_total_stats}\n")
+
+    print("Let the battle begin!\n")
+    time.sleep(1)
+
+    # Determine which player attacks first
+    players = [(player1_choice, player1_total_stats), (player2_choice, player2_total_stats)]
+    random.shuffle(players)
+    first_player = players[0][0]
+    second_player = players[1][0]
+
+    print(f"{first_player.upper()} attacks first!\n")
+    time.sleep(1)
+
+    while player1_total_stats > 0 and player2_total_stats > 0:
+        # Determine the attack strength of each player
+        player1_attack = random.randint(1, player1_total_stats)
+        player2_attack = random.randint(1, player2_total_stats)
+
+        # Reduce the total base stats of the player that attacks
+        if first_player == player1_choice:
+            player2_total_stats -= player1_attack
+            print(f"{first_player.upper()} attacks with {player1_attack} strength!")
+            time.sleep(1)
+            if player2_total_stats <= 0:
+                break
+            player1_total_stats -= player2_attack
+            print(f"{second_player.upper()} counterattacks with {player2_attack} strength!")
+            time.sleep(1)
+            if player1_total_stats <= 0:
+                break
+        else:
+            player1_total_stats -= player2_attack
+            print(f"{first_player.upper()} attacks with {player2_attack} strength!")
+            time.sleep(1)
+            if player1_total_stats <= 0:
+                break
+            player2_total_stats -= player1_attack
+            print(f"{second_player.upper()} counterattacks with {player1_attack} strength!")
+            time.sleep(1)
+            if player2_total_stats <= 0:
+                break
+
+    print("\nThe battle is over!\n")
+    time.sleep(1)
+
+    if player1_total_stats > player2_total_stats:
+        print(f"{first_player.upper()} wins!")
+    elif player2_total_stats > player1_total_stats:
+        print(f"{second_player.upper()} wins!")
+    else:
+        print("It's a tie!")
+else:
+    print("Invalid Pokemon choice for one or both players. Please try again.") 
      
 
     
